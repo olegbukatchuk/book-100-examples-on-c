8c34ed3aa8c1a8a3c541585b0137d6fbe74cc0b3
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-22 21:33:47 +0300

    ok

--- 038/main.c
+++ 038/main.c
@@ -2,4 +2,33 @@
 // Created by Oleg Bukatchuk on 22/10/2017.
 //
 
-//
+// Вычисляем НОД с использованием рекурсии
+#include <stdio.h>
+
+int hcf(int n1,int n2);
+
+int main(void) {
+
+    int n1,n2;
+
+    printf("Введите 2 положительных целых числа: ");
+    scanf("%d %d",&n1,&n2);
+    printf("НОД = %d\n",hcf(n1,n2));
+
+    return 0;
+
+}
+
+int hcf(int n1,int n2) {
+
+    if (n2!=0) {
+
+        return hcf(n2,n1%n2);
+
+    } else {
+
+        return n1;
+
+    }
+
+}
---
+++ 039/main.c
@@ -0,0 +1,63 @@
+//
+// Created by Oleg Bukatchuk on 22/10/2017.
+//
+
+// Преобразование между двоичной и десятичной системами счисления
+#include <stdio.h>
+#include <math.h>
+
+int convertBinaryToDecimal(long long n);
+long long convertDecimalToBinary(int n);
+
+int main(void) {
+
+    long long n;
+
+    printf("Введите двоичное число: ");
+    scanf("%lld",&n);
+    printf("%lld (двоичное) = %d (десятичное)\n",n,convertBinaryToDecimal(n));
+
+    printf("Введите десятичное число: ");
+    scanf("%lld",&n);
+    printf("%lld (десятичное) = %d (двоичное)\n",n,convertBinaryToDecimal(n));
+
+    return 0;
+
+}
+
+int convertBinaryToDecimal(long long n) {
+
+    int decimalNumber=0,i=0,remainder;
+
+    while (n!=0) {
+
+        remainder=n%10;
+        n/=10;
+        decimalNumber+=remainder*pow(2,i);
+        ++i;
+
+    }
+
+    return decimalNumber;
+
+}
+
+long long convertDecimalToBinary(int n) {
+
+    long long binaryNumber=0;
+    int remainder,i=1,step=1;
+
+    while (n!=0) {
+
+        remainder=n%2;
+
+        printf("Шаг %d: %d/2, Остаток = %d, Частное = %d\n",step++,n,remainder,n/2);
+        n/=2;
+        binaryNumber+=remainder*i;
+        i*=10;
+
+    }
+
+    return binaryNumber;
+
+}
