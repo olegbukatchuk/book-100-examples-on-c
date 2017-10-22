ea797e8ba7c55718e8c64d34093bde32423f159a
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-22 18:25:42 +0300

    ok

---
+++ 029/main.c
@@ -0,0 +1,47 @@
+//
+// Created by Oleg Bukatchuk on 22/10/2017.
+//
+
+// Проверяем является лт число числом Армстронга
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+    int number,originalNumber,remainder,result=0,n=0;
+
+    printf("Введите число: ");
+    scanf("%d",&number);
+
+    originalNumber=number;
+
+    while (originalNumber!=0) {
+
+        originalNumber/=10;
+        ++n;
+
+    }
+
+    originalNumber=number;
+
+    while (originalNumber!=0) {
+
+        remainder=originalNumber%10;
+        result+=pow(remainder,n);
+        originalNumber/=10;
+
+    }
+
+    if (result==number) {
+
+        printf("%d - число Армстронга\n",number);
+
+    } else {
+
+        printf("%d - не является числом Армстронга\n",number);
+
+    }
+
+    return 0;
+
+}
