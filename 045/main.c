48540c7e43eb3dcdc65f1047559a2c72226525e5
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-22 22:42:38 +0300

    ok

---
+++ 045/main.c
@@ -0,0 +1,52 @@
+//
+// Created by Oleg Bukatchuk on 22/10/2017.
+//
+
+// Передача массива функции
+#include <stdio.h>
+#include <math.h>
+
+float calculateSD(float data[]);
+
+int main(void) {
+
+    int i;
+    float data[10];
+
+    printf("Введите 10 элементов: ");
+
+    for (i=0;i<10;++i) {
+
+        scanf("%f",&data[i]);
+
+    }
+
+    printf("\nСреднеквадратичное отклонение = %.6f\n",calculateSD(data));
+
+    return 0;
+
+}
+
+float calculateSD(float data[]) {
+
+    float sum=0.0,mean,standardDeviation=0.0;
+
+    int i;
+
+    for (i=0;i<10;++i) {
+
+        sum+=data[i];
+
+    }
+
+    mean=sum/10;
+
+    for (i=0;i<10;++i) {
+
+        standardDeviation+=pow(data[i]-mean,2);
+
+    }
+
+    return sqrt(standardDeviation/10);
+
+}
